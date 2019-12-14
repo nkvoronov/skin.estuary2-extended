26f37e189a4471bb1c01c09e46f7642d3185d4d8
Nikolay Voronov <nkvoronov@gmail.com>
2019-12-14 22:44:08 +0200

    Update

---
+++ scripts/context_menu_jumptoletter.py
@@ -0,0 +1,9 @@
+import xbmc
+import xbmcgui
+
+home = xbmcgui.Window(10000)
+skin = home.getProperty("CurrentSkin")
+if skin == "skin.estuary2-extended-s":
+    xbmc.executebuiltin("SetFocus(8000)")
+else:
+    pass
